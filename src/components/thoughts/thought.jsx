eabc91cab444bcece875b2a80dbeba2ed26c9d19
Alejandro Melara <alejandro.melara96@gmail.com>
2023-05-15 23:16:22 -0400

    Create a link between thought-list and authors

--- src/components/thoughts/thought.jsx
+++ src/components/thoughts/thought.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Thought({ thought }) {
   return (
     <li className="font-medium">
       {thought.thought}
       <small className="block space-x-2 italic">
-        <span>{thought.author}</span>
+        <Link to={`/thoughts/${thought.author}`}>{thought.author}</Link>
         <span>{thought.date}</span>
         <span>{thought.time}</span>
       </small>
