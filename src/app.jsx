d2ea492f65e2084f4a48f11ee984f47ffacf3a46
Alejandro Melara <alejandro.melara96@gmail.com>
2023-05-15 23:14:04 -0400

    Create Route paths, with another format

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,33 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Error from "./components/error";
 import Layout from "./components/layout";
 import { About, Contact, Home, LoginRegister } from "./pages";
+import { loadThoughts } from "./pages/loaders";
 
-const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    children: [
-      { path: "", element: <Home /> },
-      { path: "about", element: <About /> },
-      { path: "contact", element: <Contact /> },
-      { path: "login", element: <LoginRegister /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="/" element={<Home />} loader={loadThoughts} />
+      <Route path="thoughts/:author" element={<Home />} loader={loadThoughts} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/login" element={<LoginRegister />} />
+      <Route path="*" element={<Error />} />
+    </Route>
+  )
+);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
