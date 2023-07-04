7adf0b3ccdf33b3a66bc53d774ce9960643f572a
Alejandro Melara <alejandro.melara96@gmail.com>
2023-07-04 09:35:08 -0400

    feat: :zap: Adding Outlet-Context
    This allows us to send information to rest of the child nodes and use that information

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
+import useCurrentUser from "../hooks/useCurrentUser";
 import Header from "./header/header";
 
 export default function Layout() {
+const [currentUser, setCurrentUser] = useCurrentUser()
+
   return (
     <>
-      <Header />
+      <Header user={currentUser} setUser={setCurrentUser}/>
       <main className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
-        <Outlet />
+        <Outlet context={[currentUser, setCurrentUser]}/>
       </main>
     </>
   );
