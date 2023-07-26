9f9a01527d85957922c024eef4b104c684e739f2
Alejandro Melara <alejandro.melara96@gmail.com>
2023-07-25 20:59:25 -0400

    feat: :sparkles: Delete and Edit buttons

--- src/components/thoughts/thought.jsx
+++ src/components/thoughts/thought.jsx
@@ -1,6 +1,7 @@
-import { Link } from "react-router-dom";
+import { Link, useOutletContext } from "react-router-dom";
 
 export default function Thought({ thought }) {
+  const [currentUser] = useOutletContext();
   return (
     <li className="font-medium">
       {thought.thought}
@@ -8,6 +9,13 @@ export default function Thought({ thought }) {
         <Link to={`/thoughts/${thought.author}`}>{thought.author}</Link>
         <span>{thought.date}</span>
         <span>{thought.time}</span>
+
+        {currentUser === thought.author && (
+          <div className="my-2 space-x-2">
+            <button className="rounded bg-yellow-500 px-4 py-2">Edit</button>
+            <button className="rounded bg-red-500 px-4 py-2">Delete</button>
+          </div>
+        )}
       </small>
     </li>
   );
