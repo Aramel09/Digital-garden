7e8873ca7e5bd714dc960ee8dcf276b9a7a1d140
Alejandro Melara <alejandro.melara96@gmail.com>
2023-08-16 15:57:51 -0400

    fix: :bug: Thoughts don't stay in the input after submitting
    Implement "navigation.state"

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,24 +1,44 @@
-import { Suspense } from "react";
-import { Await, Form, useLoaderData } from "react-router-dom";
+import { Suspense, useEffect, useRef } from "react";
+import { Await, Form, useLoaderData, useNavigation } from "react-router-dom";
 import Error from "../components/error";
 import { TextInput } from "../components/form";
 import Loading from "../components/loading";
 import ThoughtList from "../components/thoughts/thought-list";
+import useError from "../hooks/useErrorShown";
 import useSetCurrentUSer from "../hooks/useSetCurrentUser";
 
 export default function Home() {
+  const formRef = useRef(null);
+
+  useEffect(() => {
+    const form = formRef.current;
+    if (!form) return;
+
+    if (!error && isIdle) formRef.current.reset();
+
+    const input = form.elements.namedItem("thought");
+    if (!input) return;
+
+    input.focus();
+  });
+
   const { thoughts } = useLoaderData();
+  const navigation = useNavigation();
   const currentUser = useSetCurrentUSer();
+
+  const { error, isShowingError } = useError;
+  const isIdle = navigation.state === "idle";
 
   return (
     <>
       {currentUser && (
-        <Form method="post">
+        <Form method="post" ref={formRef}>
           <h1>{currentUser}</h1>
           <TextInput
             id="thought"
             placeholder={"What are you thinking?" + currentUser}
           />
+          {error && isShowingError && <p className="error">{error}</p>}
           <button type="submit" className="btn">
             Submit
           </button>
