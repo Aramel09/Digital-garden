f60b459955076a7b5e64ceb18e15c610e9c62a4e
Alejandro Melara <alejandro.melara96@gmail.com>
2023-08-30 20:18:12 -0400

    fix: :bug: Clear input after edit

--- src/hooks/use-clear-and-focus.js
+++ src/hooks/use-clear-and-focus.js
@@ -1,16 +1,21 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function useClearForm(error, isIdle) {
+  const [thought2Edit, setThought2Edit] = useState(null);
+
   const formRef = useRef(null);
 
   useEffect(() => {
     const form = formRef.current;
     if (!form) return;
 
-    if (!error && isIdle) formRef.current.reset();
+    if (!error && isIdle) {
+      formRef.current.reset();
+      setThought2Edit(null);
+    }
 
     form.elements.namedItem("thought").focus();
-  });
+  }, [setThought2Edit, error, isIdle]);
 
-  return formRef;
+  return { formRef, thought2Edit, setThought2Edit };
 }
